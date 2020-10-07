1f32795a888be0ab3d65364f63fa14610885390c
Kiran <kiran@opsmx.io>
2020-10-07 09:49:52 +0530

    initial commit

---
+++ src/main/java/com/demo/SbWeb1Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWeb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWeb1Application.class, args);
+    }
+
+}
